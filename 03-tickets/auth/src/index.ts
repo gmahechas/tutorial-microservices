3427f3ade50df07878f8bb3d68b94598ec9438be
gmahechas <gmahechas@ingenieros.com>
2020-09-17 11:03:31 -0500

    create user

--- 03-tickets/auth/src/index.ts
+++ 03-tickets/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongosee from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { singinRouter } from './routes/singin';
@@ -26,6 +27,20 @@ app.get('*', async (rew, res, next) => {
 // Middlewares
 app.use(errorHanldler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
-});
+const start = async () => {
+  try {
+    await mongosee.connect("mongodb+srv://tavogus:60myC0ZzX98aMUCb@microservices.lbqk1.mongodb.net/microservices?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+  } catch (error) {
+    console.log('Error:::', error);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000')
+  });
+};
+
+start();
