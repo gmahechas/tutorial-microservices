333036caa65ebb7350329529f6f5715207de6d56
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-11-24 00:08:09 -0500

    Updates

--- 03-tickets/common/src/index.ts
+++ 03-tickets/common/src/index.ts
@@ -1,3 +1,4 @@
+export * from './events/types/order-status';
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
